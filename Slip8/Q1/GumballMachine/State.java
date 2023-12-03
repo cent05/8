0a603e0bfa08129c142516b4fd72ff79e1ab3c2a
pcccs <133585729+pcccs@users.noreply.github.com>
2023-12-03 22:08:34 +0530

    Add files via upload

---
+++ Slip8/Q1/GumballMachine/State.java
@@ -0,0 +1,10 @@
+package GumballMachine;
+
+public interface State 
+{
+    public void insertQuarter();
+    public void ejectQuarter(); 
+    public void turnCrank();
+    public void dispense();
+    public void refill();
+} 
